4f13f13da14f04eaf0bc70f23feefd9844af2ea7
kshitij065 <129906291+kshitij065@users.noreply.github.com>
2023-04-05 15:18:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const names = ['pikachu', 'Jane', 'Mike', 'Sarah', 'David', 'Emily', 'Chris', 'Emma', 'James', 'Olivia'];
 const images = [
-  'https://via.placeholder.com/200',
+  'https://imgs.search.brave.com/v3U0ub1R103fmMgrYklwZMrIlpnhlMrZbI5UUQpV3hw/rs:fit:1080:1080:1/g:ce/aHR0cHM6Ly93d3cu/bmludGVuZG93b3Js/ZHJlcG9ydC5jb20v/bWVkaWEvMzgxODMv/NC8xLmpwZw',
   'https://via.placeholder.com/200/FF0000/FFFFFF',
   'https://via.placeholder.com/200/0000FF/FFFFFF',
   'https://via.placeholder.com/200/FFFF00/000000',
