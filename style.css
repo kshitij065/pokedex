875cc87dbeced914928bb9ad71f011699058883b
kshitij065 <kshitijkumar0282@gmail.com>
2023-04-05 15:05:10 +0530

    third commit

---
+++ style.css
@@ -0,0 +1,16 @@
+const names = ['John', 'Jane', 'Mike', 'Sarah', 'David', 'Emily', 'Chris', 'Emma', 'James', 'Olivia'];
+const images = [
+  'https://via.placeholder.com/200',
+  'https://via.placeholder.com/200/FF0000/FFFFFF',
+  'https://via.placeholder.com/200/0000FF/FFFFFF',
+  'https://via.placeholder.com/200/FFFF00/000000',
+  'https://via.placeholder.com/200/00FF00/000000',
+  'https://via.placeholder.com/200/FFFFFF/000000'
+];
+
+function generate() {
+  const randomName = names[Math.floor(Math.random() * names.length)];
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+  document.getElementById('random-name').textContent = randomName;
+  document.getElementById('random-image').setAttribute('src', randomImage);
+}
