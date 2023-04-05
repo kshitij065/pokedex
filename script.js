5b5382116072f880bac87c4ce3669f496c295504
kshitij065 <129906291+kshitij065@users.noreply.github.com>
2023-04-05 15:39:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-header {
-  background-color: #f44336;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100px;
+const pokemonList = [
+  {
+    name: 'Bulbasaur',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+    type: ['Grass', 'Poison'],
+    description: 'A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.'
+  },
+  {
+    name: 'Charmander',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+    type: ['Fire'],
+    description: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.'
+  },
+  {
+    name: 'Squirtle',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+    type: ['Water'],
+    description: 'When it retracts its long neck into its shell, it squirts out water with vigorous force.'
+  }
+];
+
+const pokemonListElement = document.getElementById('pokemon-list');
+
+function renderPokemonList() {
+  pokemonList.forEach(pokemon => {
+    const pokemonElement = document.createElement('li');
+    pokemonElement.innerHTML = `
+      <img src="${pokemon.image}" alt="${pokemon.name}">
+      <h2>${pokemon.name}</h2>
+      <p>Type: ${pokemon.type.join(', ')}</p>
+      <p>${pokemon.description}</p>
+    `;
+    pokemonListElement.appendChild(pokemonElement);
+  });
 }
 
-main {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-li {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 10px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  min-width: 200px;
-}
-
-img {
-  width: 150px;
-  height: 150px;
-}
-
-h2 {
-  margin: 10px 0 5px;
-  font-size: 24px;
-}
-
-p {
-  margin: 5px 0;
-}
+renderPokemonList();
