b8db97af8965896f5251283b20ddb584550b016f
kshitij065 <129906291+kshitij065@users.noreply.github.com>
2023-04-05 15:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const names = ['pikachu', 'Jane', 'Mike', 'Sarah', 'David', 'Emily', 'Chris', 'Emma', 'James', 'Olivia'];
-const images = [
-  'https://imgs.search.brave.com/v3U0ub1R103fmMgrYklwZMrIlpnhlMrZbI5UUQpV3hw/rs:fit:1080:1080:1/g:ce/aHR0cHM6Ly93d3cu/bmludGVuZG93b3Js/ZHJlcG9ydC5jb20v/bWVkaWEvMzgxODMv/NC8xLmpwZw',
-  'https://via.placeholder.com/200/FF0000/FFFFFF',
-  'https://via.placeholder.com/200/0000FF/FFFFFF',
-  'https://via.placeholder.com/200/FFFF00/000000',
-  'https://via.placeholder.com/200/00FF00/000000',
-  'https://via.placeholder.com/200/FFFFFF/000000'
-];
-
-function generate() {
-  const randomName = names[Math.floor(Math.random() * names.length)];
-  const randomImage = images[Math.floor(Math.random() * images.length)];
-  document.getElementById('random-name').textContent = randomName;
-  document.getElementById('random-image').setAttribute('src', randomImage);
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #f44336;
+  color: #fff;
+  font-size: 36px;
 }
+
+main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+li {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  min-width: 200px;
+}
+
+img {
+  width: 150px;
+  height: 150px;
+}
+
+h2 {
+  margin: 10px 0 5px;
+  font-size: 24px;
+}
+
+p {
+  margin: 5px 0;
+}
+
